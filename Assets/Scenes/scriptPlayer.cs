b3248f8f7ecae058ea2d1784c079a8a5c372d737
kader <karambirikader73@gmail.com>
2024-05-27 13:41:59 -0300

    Initial commit

---
+++ Assets/Scenes/scriptPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // good management of the physic
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * 5f * Time.fixedDeltaTime * Input.GetAxis("Vertical"));
+        transform.Translate(Vector3.right * 5f * Time.fixedDeltaTime * Input.GetAxis("Horizontal"));
+    }
+}
